ecfa40e1374891eafbd8682d2b2db91abf23d14e
mohammadalmoqdad <almekad.mohammad@gmail.com>
2021-01-13 09:24:34 +0200

    lab13 without tests

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+require('dotenv');
 const routes = require('./src/auth/routes.js')
+const extra_routes = require('./extra-route.js');
+
 app.use(cors());
 app.use(express.static('./public'));
 app.use(express.json())
 app.use(routes);
+app.use(extra_routes);
 
 module.exports = {
     app: app,
